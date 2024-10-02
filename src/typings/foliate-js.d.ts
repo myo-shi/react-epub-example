28b1871831c42399c30e83e56c1aa489a6602920
myo <aokage000@gmail.com>
2024-10-02 17:51:48 +0900

    test annotation

--- src/typings/foliate-js.d.ts
+++ src/typings/foliate-js.d.ts
@@ -1,27 +1,44 @@
 declare namespace JSX {
-	import type { View } from "@/lib/foliate-js/view";
-	interface IntrinsicElements {
-		"foliate-view": JSX.HTMLAttribus<View>;
-	}
+  import type { View } from "@/lib/foliate-js/view";
+  interface IntrinsicElements {
+    "foliate-view": JSX.HTMLAttribus<View>;
+  }
+}
+
+declare module "@/lib/foliate-js/epubcfi" {
+  export class CFI { }
+}
+
+declare module "@/lib/foliate-js/overlayer" {
+  type Rects = { top?: number; bottom?: number; left?: number; right?: number; height?: number; width?: number }
+
+  export class Overlayer {
+    static underline(rects: Rects, options?: any): void;
+  }
 }
 
 declare module "@/lib/foliate-js/epub" {
-	export class EPUB {}
+  export class EPUB { }
 }
 
 declare module "@/lib/foliate-js/view" {
-	import type { EPUB } from "@/lib/foliate-js/epub";
+  import type { EPUB } from "@/lib/foliate-js/epub";
+  import type { CFI } from 'lib/foliate-js/epubcfi'
 
-	interface Paginator extends HTMLElement {
-		next(): Promise<void>;
-	}
+  type Annotation = {
+    value: CFI;
+  }
 
-	export class View extends HTMLElement {
-		renderer: Paginator;
-		// book: File;
-		// prev(): Promise<void>;
-		open(book: EPUB): Promise<void>;
-		goLeft(): Promise<void>;
-		goRight(): Promise<void>;
-	}
+  interface Paginator extends HTMLElement {
+    next(): Promise<void>
+  }
+
+  export class View extends HTMLElement {
+    renderer: Paginator;
+    open(book: EPUB): Promise<void>;
+    goLeft(): Promise<void>;
+    goRight(): Promise<void>;
+    getCFI(index: number, range?: Range): CFI
+    addAnnotation(annotation: Annotation, remove?: boolean): Promise<void>
+  }
 }
