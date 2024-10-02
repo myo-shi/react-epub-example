d9a385243eaf1042d4401a28ecfcc34a3ee91467
myo <aokage000@gmail.com>
2024-10-02 23:21:13 +0900

    format

--- src/modules/reader/Reader.tsx
+++ src/modules/reader/Reader.tsx
@@ -1,105 +1,105 @@
 import { Button } from "@/components/ui/button";
+import type { View } from "@/lib/foliate-js/view";
 import { useEffect, useRef } from "react";
 import { Reader } from "./reader";
 import styles from "./styles.module.css";
-import type { View } from "@/lib/foliate-js/view";
 
 type ReaderProps = {
-  bookFile: File;
-  onClose: () => void;
+	bookFile: File;
+	onClose: () => void;
 };
 
 const reader = new Reader();
 
 window.addEventListener("selectionchange", (event) => {
-  console.log("selectionchange", window.getSelection());
+	console.log("selectionchange", window.getSelection());
 });
 
 export function ReaderComp({ bookFile, onClose }: ReaderProps) {
-  const viewerRef = useRef<View>(
-    document.getElementById("foliate-view") as unknown as View,
-  );
-  useEffect(() => {
-    reader.open(bookFile, viewerRef);
-  }, [bookFile]);
+	const viewerRef = useRef<View>(
+		document.getElementById("foliate-view") as unknown as View,
+	);
+	useEffect(() => {
+		reader.open(bookFile, viewerRef);
+	}, [bookFile]);
 
-  // const { iframeWindow, displayed } = useReaderSnapshot();
+	// const { iframeWindow, displayed } = useReaderSnapshot();
 
-  // const handleClose = () => {
-  // 	readerActions.close();
-  // 	onClose();
-  // };
+	// const handleClose = () => {
+	// 	readerActions.close();
+	// 	onClose();
+	// };
 
-  // useEffect(() => {
-  // 	if (displayed) return;
-  // 	readerActions.openBook(bookFile).then(() => {
-  // 		readerActions.render(viewerRef.current);
-  // 	});
-  // }, [bookFile, displayed]);
+	// useEffect(() => {
+	// 	if (displayed) return;
+	// 	readerActions.openBook(bookFile).then(() => {
+	// 		readerActions.render(viewerRef.current);
+	// 	});
+	// }, [bookFile, displayed]);
 
-  // useEffect(() => {
-  // 	const onWheel = throttle((event: WheelEvent) => {
-  // 		event.preventDefault();
-  // 		const deltaY = Math.sign(event.deltaY);
-  // 		const deltaX = Math.sign(event.deltaX);
-  // 		if (deltaY > -1 || deltaX > 0) {
-  // 			readerActions.goNext();
-  // 		} else {
-  // 			readerActions.goPrev();
-  // 		}
-  // 	}, 49);
-  // 	iframeWindow?.addEventListener("wheel", onWheel, {});
-  // 	// While re-rendering, the wheel event is captured by the parent window because there is no iframe window.
-  // 	window.document.addEventListener("wheel", onWheel);
+	// useEffect(() => {
+	// 	const onWheel = throttle((event: WheelEvent) => {
+	// 		event.preventDefault();
+	// 		const deltaY = Math.sign(event.deltaY);
+	// 		const deltaX = Math.sign(event.deltaX);
+	// 		if (deltaY > -1 || deltaX > 0) {
+	// 			readerActions.goNext();
+	// 		} else {
+	// 			readerActions.goPrev();
+	// 		}
+	// 	}, 49);
+	// 	iframeWindow?.addEventListener("wheel", onWheel, {});
+	// 	// While re-rendering, the wheel event is captured by the parent window because there is no iframe window.
+	// 	window.document.addEventListener("wheel", onWheel);
 
-  // 	return () => {
-  // 		iframeWindow?.removeEventListener("wheel", onWheel);
-  // 		window.document.removeEventListener("wheel", onWheel);
-  // 	};
-  // }, [iframeWindow]);
+	// 	return () => {
+	// 		iframeWindow?.removeEventListener("wheel", onWheel);
+	// 		window.document.removeEventListener("wheel", onWheel);
+	// 	};
+	// }, [iframeWindow]);
 
-  return (
-    <div className={`flex h-full w-full overflow-hidden ${styles.black}`}>
-      <div className="flex h-full w-full flex-1 flex-col bg-zinc-950">
-        <div
-          style={{
-            flex: 1,
-            display: "flex",
-            minHeight: "100px",
-            width: "100%",
-            overflow: "hidden",
-          }}
-        >
-          <div className="flex items-center">
-            <Button
-              variant={"ghost"}
-              type="button"
-              className="h-52"
-            // onClick={() => readerActions.goPrev()}
-            >
-              &#60;
-            </Button>
-          </div>
-          <foliate-view
-            ref={viewerRef}
-            id="foliate-view"
-            style={{ width: "100%", flex: 1, background: "white" }}
-          />
-          <div className="flex items-center">
-            <Button
-              variant={"ghost"}
-              type="button"
-              className="h-52"
-            // onClick={() => readerActions.goNext()}
-            >
-              &#62;
-            </Button>
-          </div>
-        </div>
-        <div id="footer" className="flex w-full justify-between" />
-      </div>
-    </div>
-  );
+	return (
+		<div className={`flex h-full w-full overflow-hidden ${styles.black}`}>
+			<div className="flex h-full w-full flex-1 flex-col bg-zinc-950">
+				<div
+					style={{
+						flex: 1,
+						display: "flex",
+						minHeight: "100px",
+						width: "100%",
+						overflow: "hidden",
+					}}
+				>
+					<div className="flex items-center">
+						<Button
+							variant={"ghost"}
+							type="button"
+							className="h-52"
+							// onClick={() => readerActions.goPrev()}
+						>
+							&#60;
+						</Button>
+					</div>
+					<foliate-view
+						ref={viewerRef}
+						id="foliate-view"
+						style={{ width: "100%", flex: 1, background: "white" }}
+					/>
+					<div className="flex items-center">
+						<Button
+							variant={"ghost"}
+							type="button"
+							className="h-52"
+							// onClick={() => readerActions.goNext()}
+						>
+							&#62;
+						</Button>
+					</div>
+				</div>
+				<div id="footer" className="flex w-full justify-between" />
+			</div>
+		</div>
+	);
 }
 
 // type HeaderProps = {
--- src/typings/foliate-js.d.ts
+++ src/typings/foliate-js.d.ts
@@ -1,53 +1,65 @@
 declare namespace JSX {
-  import type { View } from "@/lib/foliate-js/view";
-  interface IntrinsicElements {
-    "foliate-view": JSX.HTMLAttribus<View>;
-  }
+	import type { View } from "@/lib/foliate-js/view";
+	interface IntrinsicElements {
+		"foliate-view": JSX.HTMLAttribus<View>;
+	}
 }
 
 declare module "@/lib/foliate-js/epubcfi" {
-  export type CFI = `epubcfi(${string})`;
+	export type CFI = `epubcfi(${string})`;
 }
 
 declare module "@/lib/foliate-js/overlayer" {
-  type Rects = { top?: number; bottom?: number; left?: number; right?: number; height?: number; width?: number }
+	type Rects = {
+		top?: number;
+		bottom?: number;
+		left?: number;
+		right?: number;
+		height?: number;
+		width?: number;
+	};
 
-  export class Overlayer {
-    static underline(rects: Rects, options?: any): void;
-    static highlight(rects: Rects, options?: any): void;
-  }
+	export class Overlayer {
+		get element(): HTMLElement;
+		static underline(rects: Rects, options?: any): void;
+		static highlight(rects: Rects, options?: any): void;
+	}
 }
 
 declare module "@/lib/foliate-js/epub" {
-  export class EPUB { }
+	export class EPUB {}
 }
 
 declare module "@/lib/foliate-js/view" {
-  import type { EPUB } from "@/lib/foliate-js/epub";
-  import type { CFI } from '@/lib/foliate-js/epubcfi'
+	import type { EPUB } from "@/lib/foliate-js/epub";
+	import type { CFI } from "@/lib/foliate-js/epubcfi";
 
-  type Annotation = {
-    value: string;
-    color?: string;
-  }
+	type Annotation = {
+		value: string;
+		color?: string;
+	};
 
-  interface Paginator extends HTMLElement {
-    next(): Promise<void>
-  }
+	interface Paginator extends HTMLElement {
+		next(): Promise<void>;
+	}
 
-  // https://github.com/vaadin/web-components/issues/350
-  interface FoliateViewElementEventMap {
-    "load": CustomEvent<{ index: number; doc: Document }>;
-    "draw-annotation": CustomEvent<{ draw: any; annotation: any }>;
-  }
+	// https://github.com/vaadin/web-components/issues/350
+	interface FoliateViewElementEventMap {
+		load: CustomEvent<{ index: number; doc: Document }>;
+		"draw-annotation": CustomEvent<{ draw: any; annotation: any }>;
+	}
 
-  export class View extends HTMLElement {
-    renderer: Paginator;
-    open(book: EPUB): Promise<void>;
-    goLeft(): Promise<void>;
-    goRight(): Promise<void>;
-    getCFI(index: number, range?: Range): CFI
-    addAnnotation(annotation: Annotation, remove?: boolean): Promise<void>
-    addEventListener<K extends keyof FoliateViewElementEventMap>(type: K, listener: (this: HTMLElement, ev: FoliateViewElementEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
-  }
+	export class View extends HTMLElement {
+		renderer: Paginator;
+		open(book: EPUB): Promise<void>;
+		goLeft(): Promise<void>;
+		goRight(): Promise<void>;
+		getCFI(index: number, range?: Range): CFI;
+		addAnnotation(annotation: Annotation, remove?: boolean): Promise<void>;
+		addEventListener<K extends keyof FoliateViewElementEventMap>(
+			type: K,
+			listener: (this: HTMLElement, ev: FoliateViewElementEventMap[K]) => any,
+			options?: boolean | AddEventListenerOptions,
+		): void;
+	}
 }
